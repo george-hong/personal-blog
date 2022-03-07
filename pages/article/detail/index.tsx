865075b9b6f198e4e776e2711092555e89975d28
a916856595 <hcj147@vip.qq.com>
2022-03-07 22:36:26 +0800

    feat: add query parameter demo

--- pages/article/detail/index.tsx
+++ pages/article/detail/index.tsx
@@ -1,15 +1,19 @@
-import type { NextPage } from 'next'
-import Link from 'next/link'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 const ArticleDetail: NextPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
   return (
     <div>
       <Head>
         <title>article detail</title>
         <meta name="personal-blog" content="文章详情"/>
       </Head>
-      article detail
+      article detail id: { id }
       <Link href="/">
         <a>back home</a>
       </Link>
