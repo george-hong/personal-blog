8d818a5d94862657ccb83d77755ae510f6312cb1
george-hong <hcj147@vip.qq.com>
2022-11-21 22:43:28 +0800

    refactor: Update deployment script.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name   : "personal-blog",
+      name   : "personal-blog-service",
       script : "node_modules/next/dist/bin/next",
       args: "start",
       instances: 1,
